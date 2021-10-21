9f78d9fc850c65dbb392cc8a2f14fc08388cbfec
JCJ <jesse.jackson.c@gmail.com>
2021-10-21 11:02:10 -0500

    Implemented ability to send images

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 export default function MessageForm(props) {
   const [value, setValue] = useState('');
@@ -22,6 +23,10 @@ export default function MessageForm(props) {
     isTyping(props, chatId);
   };
 
+  const handleUpload = (e) => {
+    sendMessage(creds, chatId, { files: e.target.files, text: '' });
+  };
+
   return (
     <form className='message-form' onSubmit={handleSubmit}>
       <input
@@ -31,6 +36,21 @@ export default function MessageForm(props) {
         onChange={handleChange}
         onSubmit={handleSubmit}
       />
+      <label htmlFor='upload-button'>
+        <span className='image-button'>
+          <PictureOutlined className='picture-icon' />
+        </span>
+      </label>
+      <input
+        type='file'
+        multiple={false}
+        id='upload-button'
+        style={{ display: 'none' }}
+        onChange={handleUpload}
+      />
+      <button type='submit' className='send-button'>
+        <SendOutlined className='send-icon' />
+      </button>
     </form>
   );
 }
