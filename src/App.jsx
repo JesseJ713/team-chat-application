1b13c0d104f92a5c7b544c36148520a37e93137f
JCJ <jesse.jackson.c@gmail.com>
2021-10-21 12:44:19 -0500

    Validation showing LoginForm on initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { ChatEngine } from 'react-chat-engine';
+
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './app.css';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height='100vh'
