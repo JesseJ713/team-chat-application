a3b78431307fb0d71be6c6e3149e992a3e72f4a1
JCJ <jesse.jackson.c@gmail.com>
2021-10-22 00:52:58 -0500

    Added audio feedback to new message submission

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,11 @@ const App = () => {
       userName={localStorage.getItem('username')}
       userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3'
+        ).play()
+      }
     />
   );
 };
